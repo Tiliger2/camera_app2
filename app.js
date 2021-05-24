eecc86fee233218cc8dad0e1e586b18f48cce068
Tiliger2 <74723760+Tiliger2@users.noreply.github.com>
2021-05-24 21:09:34 +0530

    2nd

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ function cameraStart() {
 }
 async function getDevices() {
   const devices = await navigator.mediaDevices.enumerateDevices();
+  document.getElementById("demo").innerHTML = devices;
 }
 // Take a picture when cameraTrigger is tapped
 cameraTrigger.onclick = function() {

d58adcc9ef19204013250fdc66659a874da83047
Tiliger2 <74723760+Tiliger2@users.noreply.github.com>
2021-05-24 20:45:11 +0530

    2nd

--- app.js
+++ app.js
@@ -30,3 +30,4 @@ cameraTrigger.onclick = function() {
 };
 // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
+window.addEventListener("load", getDevices, false);
